4692ad37021e57a3ce2efbe9d1fb5a1662b21c89
Taki <javajavawookjavawook@gmail.com>
2022-12-06 00:10:26 +0900

    refactor: テストケースを整理

--- target_project/test/FoolishCoderTest.cpp
+++ target_project/test/FoolishCoderTest.cpp
@@ -5,15 +5,31 @@
  */
 
 #include "gtest/gtest.h"
-#include "FoolishCoder.h"
+#include "UnExistFile.h"
+#include "UnExistClass.h"
 
 namespace foolish_coder
 {
-    // 存在しない静的メソッドが実行された場合のテスト
-    TEST(FoolishCoderTest, CreateHeaderFileTest)
+    // 存在しないファイルがincludeされた場合のテスト
+    TEST(UnExistFileTest, CreateUnExistFileTest){}
+
+    // 存在しないクラスがインスタンス化された場合のテスト
+    TEST(UnExistClassTest, CreateUnExistClassTest)
     {
+        UnExistClass obj;
+    }
+
+    // 存在しないメソッドが呼び出された場合のテスト
+    TEST(UnExistMethodTest, CreateUnExistMethodTest){
+        UnExistClass obj;
+        obj.unExistMethod();
+    }
+
+    // 存在しない0を返すメソッドが呼び出された場合のテスト
+    TEST(ReturnValueMethodTest, CreateReturnValueMethodTest){
+        UnExistClass obj;
+        int acutual = obj.getValue();
         int expected = 0;
-        FoolishCoder coder;
-        coder.start();
+        EXPECT_EQ(acutual, expected);
     }
 }
