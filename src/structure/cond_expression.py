2ce707546a3e07e8f882aa571266cc15e5090648
Taki <javajavawookjavawook@gmail.com>
2022-12-18 16:29:55 +0900

    update: 自動生成で不用意に適用可能範囲を広げてしまう処理を削除

--- src/structure/cond_expression.py
+++ src/structure/cond_expression.py
@@ -15,12 +15,13 @@ class CondExpression:
         cond_expression_code = indent
         cond_expression_code += "if" if branch == 0 else "else if"
         if type(min) == str or self.min == self.max:
-            if branch == 0 and not is_last:
-                cond_expression_code += f"({arg_name} <= {self.min})"
-            elif branch > 0 and is_last:
-                cond_expression_code += f"({self.max} <= {arg_name})"
-            else:
-                cond_expression_code += f"({arg_name} == {self.min})"
+            cond_expression_code += f"({arg_name} == {self.min})"
+            # if branch == 0 and not is_last:
+            #     cond_expression_code += f"({arg_name} <= {self.min})"
+            # elif branch > 0 and is_last:
+            #     cond_expression_code += f"({self.max} <= {arg_name})"
+            # else:
+            #     cond_expression_code += f"({arg_name} == {self.min})"
         else:
             cond_expression_code += f"({self.min} <= {arg_name} && {arg_name} <= {self.max})"
         cond_expression_code += "{\n"
